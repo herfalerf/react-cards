d4533cd1addcf3e514152c3fa14a410188333bbf
herfalerf <jpendy@gmail.com>
2021-06-08 21:08:09 -0500

    added smoke and snapshot tests

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Card from "./Card";
 
 it("renders App", () => {
-  render(<App />);
+  render(<Card />);
 });
 
 it("matches snapshot", function () {
-  const { asFragment } = render(<App />);
+  const { asFragment } = render(<Card />);
 });
--- src/Deck.test.js
+++ src/Deck.test.js
@@ -1,10 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Deck from "./Deck";
 
 it("renders App", () => {
-  render(<App />);
+  render(<Deck />);
 });
 
 it("matches snapshot", function () {
-  const { asFragment } = render(<App />);
+  const { asFragment } = render(<Deck />);
 });
