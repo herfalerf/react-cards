b385a88b45781ba39d3d700aefaa4595293cb396
herfalerf <jpendy@gmail.com>
2021-06-08 20:54:29 -0500

    part 2 functionality finished

--- src/Deck.js
+++ src/Deck.js
@@ -1,47 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
 import axios from "axios";
 
 const Deck = () => {
   const [deck, setDeck] = useState(null);
-  const [isSending, setIsSending] = useState(false);
   const [cards, setCards] = useState([]);
-
-  const add = (cardsObj) => {
-    setCards((cards) => [...cards, cardsObj]);
-  };
+  const [draw, setDraw] = useState(false);
 
   useEffect(() => {
     async function loadDeck() {
       const deckRes = await axios.get(
         `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
       );
-      setDeck(deckRes.data.deck_id);
-      console.log(deckRes.data);
+      setDeck(deckRes.data);
     }
 
     loadDeck();
   }, [setDeck]);
 
-  console.log(`this is the deck code: ${deck}`);
-
   const getCard = async function getCard() {
-    let cardRes = await axios.get(
-      `https://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`
-    );
-    console.log(cardRes.data.cards[0]);
-    const card = cardRes.data.cards[0];
-    setCards((c) => [
-      ...c,
-      { id: card.code, name: card.value + " of " + card.suit },
-    ]);
+    try {
+      let cardRes = await axios.get(
+        `https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`
+      );
+      if (cardRes.data.remaining === 0) {
+        stopTimer();
+        throw new Error("no cards remaining!");
+      }
+
+      const card = cardRes.data.cards[0];
+      setCards((c) => [
+        ...c,
+        { id: card.code, name: card.value + " of " + card.suit },
+      ]);
+    } catch (err) {
+      alert(err);
+    }
   };
-  console.log(cards);
+
+  const timerId = useRef();
+
+  useEffect(() => {
+    if (draw === true) {
+      timerId.current = setInterval(() => {
+        getCard();
+      }, 1000);
+
+      return () => {
+        clearInterval(timerId.current);
+      };
+    } else {
+      stopTimer();
+    }
+  });
+
+  const stopTimer = () => {
+    clearInterval(timerId.current);
+  };
+
+  const toggleDraw = () => {
+    setDraw(!draw);
+  };
+
   const cardsList = cards.map((c) => <Card key={c.id} name={c.name} />);
 
   return (
     <>
-      <button onClick={getCard}>Get Card</button>
+      <button onClick={toggleDraw}>
+        {draw ? "Stop Drawing" : "Start Drawing"}
+      </button>
       <ul>
         <div>{cardsList}</div>
       </ul>
